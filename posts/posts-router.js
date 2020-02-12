e1588de379d1cba7cae4c2e5ba0542ce6e5360a3
IsabellaGuo <isabella.guofei@gmail.com>
2020-02-11 21:49:53 -0800

    POST are done.

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -72,9 +72,72 @@ router.get("/:id/comments", (req, res) => {
     });
 });
 
+//POST - /api/posts
+
+router.post("/", (req, res) => {
+    if (!(req.body && req.body.title && req.body.contents)) {
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        });
+        return true;
+    } 
+
+    Posts
+    .insert(req.body)
+    .then (post => {
+        if (post) {
+            res.status(201).json(post);
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "There was an error while saving the post to the database" 
+    });
+
+    });
+});
 
 
+//POST - /api/posts/:id/comments
 
+router.post("/:id/comments", (req, res) => {
+
+    if ( !( req.body && req.body.text )) {
+        res.status(400).json({
+            message: "Please provide text for the comment."
+        });
+        return true;
+    } 
+
+    Posts
+        .findById(req.params.id)
+		.then(result => {
+			if (result) {
+				Posts.insertComment({
+					text: req.body.text,
+					post_id: req.params.id
+				})
+					.then(result => {
+						res.status(201).json({ ...result, ...req.body });
+					})
+					.catch(err => {
+						res.status(404).json({
+							message:
+								"There was an error while saving the comment to the database"
+						});
+					});
+			} else {
+				res.status(404).json({
+					message: "The post with the specified ID does not exist."
+				});
+			}
+		})
+		.catch(err => {
+			res.status(500).json({
+				message: "There was an error while saving the comment to the database"
+			});
+		});
+})
 
 
 
